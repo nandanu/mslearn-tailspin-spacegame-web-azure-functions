8dbc5fdcf74bde6bd4a757cdd0a587caa89b5dc0
Nandan Udiavar <nandan.udiavar@gmail.com>
2022-02-11 13:00:37 -0800

    Fixed json serialization issues

--- Tailspin.SpaceGame.LeaderboardFunction/Models/LeaderboardResponse.cs
+++ Tailspin.SpaceGame.LeaderboardFunction/Models/LeaderboardResponse.cs
@@ -1,32 +1,41 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace TailSpin.SpaceGame.LeaderboardFunction
 {
     public class LeaderboardResponse
     {
         // The game mode selected in the view.
+        [JsonPropertyName("selectedMode")]
         public string SelectedMode { get; set; }
         // The game region (map) selected in the view.
+        [JsonPropertyName("selectedRegion")]
         public string SelectedRegion { get; set; }
         // The current page to be shown in the view.
+        [JsonPropertyName("page")]
         public int Page { get; set; }
         // The number of items to show per page in the view.
+        [JsonPropertyName("pageSize")]
         public int PageSize { get; set; }
 
         // The scores to display in the view.
+        [JsonPropertyName("scores")]
         public IEnumerable<ScoreProfile> Scores { get; set; }
         // The total number of results for the selected game mode and region in the view.
+        [JsonPropertyName("totalResults")]
         public int TotalResults { get; set; }
     }
 
     /// <summary>
     /// Combines a score and a user profile.
     /// </summary>
-    public struct ScoreProfile
+    public class ScoreProfile
     {
         // The player's score.
-        public Score Score;
+        [JsonPropertyName("score")]
+        public Score Score { get; set; }
         // The player's profile.
-        public Profile Profile;
+        [JsonPropertyName("profile")]
+        public Profile Profile { get; set; }
     }
 }
